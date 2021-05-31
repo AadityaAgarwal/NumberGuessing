760d784dc9127965893ec517b16a941328ef0275
AadityaAgarwal <73215428+AadityaAgarwal@users.noreply.github.com>
2021-05-31 09:45:16 +0530

    Update NumberGuessing.py

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -5,11 +5,17 @@ print("Choose a number (between 0-9)")
 
 while (chance>=0):
     inputAnswer=int (input("Enter Your Guess:- "))
-    if(inputAnswer!=ans):
+    if(inputAnswer<=7):
         chance-=1
         print("Guess a number greater then ",inputAnswer )
     elif(inputAnswer==8):
-     print("Congratulations!! You won")    
+     print("Congratulations!! You won")
+     break
+
+    elif(inputAnswer==9):
+        print("Enter a number less than 9")
+        chance-=1
     if(chance==0):
         print("You lost....the correct answer is ",ans)
+        break
  
